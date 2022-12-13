34f4a2e5d28a277c177e6d01121b33d2a7402527
Dasha <daryaishchuk@gmail.com>
2022-12-13 21:54:47 +0300

    task16

--- task16/Program.cs
+++ task16/Program.cs
@@ -12,7 +12,7 @@ int a = int.Parse(Console.ReadLine());
 Console.Write("Введите число b = ");
 int b = int.Parse(Console.ReadLine());
 
-if (a == b)
+if (a == b*b)
 
 {
     Console.Write("Да, является квадратом.");
