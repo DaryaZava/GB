49d6c447ba8cd5582f3042bd5cda33ca2180af4e
Dasha <daryaishchuk@gmail.com>
2022-12-22 14:57:20 +0300

    Пр определяет присутсвует заданное число в массиве или нет

---
+++ task33/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 33: Задайте массив.Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив[6, 7, 19, 345, 3]->нет
+// - 3; массив[6, 7, 19, 345, 3]->да
+
+int[] NewArray(int size)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(1, 5);
+    }
+    return res;
+}
+
+void PrintArray(int[] size)
+{
+    for (int j = 0; j < size.Length; j++)
+    {
+        Console.Write(size[j] + " ");
+    }
+}
+
+int[] array = NewArray(4);
+Console.Write("Введите любое число = ");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.Write(A + " ; массив [");
+PrintArray(array);
+Console.Write("] -->");
+
+int i = 0;
+int count = 0;
+while (i < array.Length)
+{
+    if (A == array[i])
+    {
+        count++;
+    }
+    i++;
+}
+if (count == 0)
+{
+    Console.Write(" нет");
+}
+else{
+    Console.Write(" да");
+}
